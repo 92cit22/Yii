cd2df4dfac36a2db08c95ec86a50990d13155a4e
NerveFalcon <suraevoleg@gmail.com>
2022-01-24 01:57:12 +0300

    Practical work 15 (with interactive)

--- web/js/15.ts
+++ web/js/15.ts
@@ -1,124 +1,240 @@
 ﻿class ElemBuilder {
-	private currentElement: HTMLElement;
-	public subBuilder: ElemBuilder;
-	public style: CSSStyleDeclaration;
+    private currentElement: HTMLElement;
+    public subBuilder: ElemBuilder;
+    public style: CSSStyleDeclaration;
 
-	public get height() {
-		return this.style.height
-	}
-	public get width() {
-		return this.style.width
-	}
+    public get element() {
+        return this.currentElement;
+    }
 
-	constructor(element: HTMLElement = null) {
-		this.currentElement = element !== null ? element : document.body;
-		this.style = this.currentElement.style;
-	}
+    public get height() {
+        return this.style.height
+    }
+    public get width() {
+        return this.style.width
+    }
 
-	createSubElement(tag: string) {
-		let elem = document.createElement(tag);
-		elem.style.margin = this.currentElement.style.margin;
-		this.subBuilder = new ElemBuilder(elem);
-		this.currentElement.appendChild(elem);
-		return this.subBuilder;
-	}
+    constructor(element: HTMLElement = null) {
+        this.currentElement = element !== null ? element : document.body;
+        this.style = this.currentElement.style;
+    }
 
-	append(elem: string | Node) {
-		this.currentElement.append(elem);
-		return this;
-	}
+    appendNewElement(tag: string) {
+        this.appendElement(this.createSubElement(tag));
+        return this.subBuilder;
+    }
 
-	setBorder() {
-		this.style.border = "1px solid black";
-		return this;
-	}
+    appendElement(elem: HTMLElement) {
+        this.subBuilder = new ElemBuilder(elem);
+        this.currentElement.appendChild(elem);
+        return this.subBuilder;
+    }
+    prependNewElement(tag: string) {
+        this.prependElement(this.createSubElement(tag));
+        return this.subBuilder;
+    }
 
-	setHeight(newHeight: string) {
-		this.style.height = newHeight;
-		return this;
-	}
+    prependElement(elem: HTMLElement) {
+        this.subBuilder = new ElemBuilder(elem);
+        this.currentElement.prepend(elem);
+        return this.subBuilder;
+    }
 
-	setWidth(newWidth: string) {
-		this.style.width = newWidth;
-		return this;
-	}
+    append(elem: string | Node) {
+        this.currentElement.append(elem);
+        return this;
+    }
 
-	setStyle(property: string, value: string | null, priority?: string) {
-		this.style.setProperty(property, value, priority);
-		return this;
-	}
+    setBorder() {
+        this.style.border = "1px solid black";
+        return this;
+    }
+
+    setHeight(newHeight: string) {
+        this.style.height = newHeight;
+        return this;
+    }
+
+    setWidth(newWidth: string) {
+        this.style.width = newWidth;
+        return this;
+    }
+
+    setStyle(property: string, value: string | null, priority?: string) {
+        this.style.setProperty(property, value, priority);
+        return this;
+    }
+
+    addClass(className: string) {
+        this.currentElement.classList.add(className);
+        return this;
+    }
+
+    appendElemSize() {
+        this.appendNewElement('p').append("Cell size " + this.width + "*" + this.height);
+        return this;
+    }
+
+    setEmpty() {
+        return this.addClass("empty")
+            .setStyle('color', 'black')
+            .setStyle('background-color', 'white')
+            .appendNewElement('p')
+            .append('empty cell size 900px*30px');
+    }
+
+    private createSubElement(tag: string) {
+        let elem = document.createElement(tag);
+        this.addEvents(elem);
+        this.subBuilder = new ElemBuilder(elem);
+        return elem;
+    }
+
+    private addEvents(elem: HTMLElement) {
+        switch (elem.tagName) {
+            case 'P':
+                elem.onmouseenter = function (event) {
+                    switchColor(event.target as HTMLElement);
+                };
+                elem.onmouseleave = function (event) {
+                    switchColor(event.target as HTMLElement);
+                };
+                break;
+            case 'DIV':
+                elem.onclick = function (event) {
+                    switchPreviousSibling(event.currentTarget as HTMLElement)
+                }
+                elem.ondblclick = function (event) {
+                    switchNextSibling(event.currentTarget as HTMLElement)
+                }
+                break;
+            default:
+        }
+    }
+}
+function switchPreviousSibling(element: HTMLElement) {
+    let sibling = element.previousElementSibling.previousElementSibling as HTMLElement;
+    if (sibling === null)
+        sibling = element.parentElement.lastElementChild as HTMLElement
+    switchSibling(element, sibling);
+}
+function switchNextSibling(element: HTMLElement) {
+    let sibling = element.nextElementSibling.nextElementSibling as HTMLElement;
+    if (sibling === null)
+        sibling = element.parentElement.firstElementChild as HTMLElement
+    switchSibling(element, sibling);
+}
+function switchSibling(element: HTMLElement, sibling: HTMLElement) {
+    let children = element.children;
+    let sibChildren = sibling.children;
+    for (var j = children.length; j > 0; j--) {
+        element.append(sibChildren[0].cloneNode(true))
+        sibChildren[0].remove()
+        sibling.append(children[0].cloneNode(true))
+        children[0].remove()
+    }
+}
+function switchColor(element: HTMLElement) {
+    let col = element.parentElement.style.color;
+    element.parentElement.style.color = element.parentElement.style.backgroundColor;
+    element.parentElement.style.backgroundColor = col;
 }
 
 let body = new ElemBuilder();
-body.setHeight("98vh");
 body.style.textAlign = "center";
 body.style.verticalAlign = "middle";
 body.style.fontSize = "16pt";
 body.style.margin = "auto";
-body.style.paddingTop = "2vh";
+body.element.innerHTML = "";
+let styles = body.appendNewElement("style");
+styles.append(`
+p{
+    width:max-content;
+    margin: auto;
+}
+div{
+    margin: auto;
+    box-sizing: border-box;
+}
+.empty{
+}
+.rotate > p{
+    transform: rotate(90deg);
+    transform-origin: left bottom;
+    margin: 30px 0;
+}
+`)
 
-let header = body.createSubElement('div');
-header.setHeight("10%")
-	.setWidth("90%")
-	.setBorder()
-	.createSubElement('p')
-	.append('Header');
+let header = body.appendNewElement('div');
+header.setHeight("125px")
+    .setBorder()
+    .setWidth("900px")
+    .setStyle("color", "red")
+    .appendElemSize()
+    .prependNewElement('p')
+    .append("Header")
+    .appendNewElement("br");
 
 
-let subHeader = body.createSubElement('div');
-subHeader.setHeight("5%")
-	.setWidth("90%")
-	.createSubElement('p')
-	.append('SubHeader');
+let subHeader = body.appendNewElement('div');
+subHeader.setHeight("30px")
+    .setWidth("900px")
+    .setEmpty();
 
-let nav = body.createSubElement('div');
-nav.setHeight("10%")
-	.setWidth("90%")
-	.setBorder()
-	.createSubElement('p')
-	.append('Navigation bar');
+let horizontal = body.appendNewElement('div');
+horizontal.setHeight("35px")
+    .setBorder()
+    .setWidth("900px")
+    .setStyle("color", "blue")
+    .appendElemSize()
+    .prependNewElement('p')
+    .append("Horizontal menu")
+    .appendNewElement("br");
 
-let subNav = body.createSubElement('div');
-subNav.setHeight("5%")
-	.createSubElement('p')
-	.append('Sub Nav');
+let subNav = body.appendNewElement('div');
+subNav.setHeight("30px")
+    .setWidth("900px")
+    .setStyle('backgroundColor', 'white')
+    .setEmpty();
 
-let composeHeight = "50%";
-let main = body.createSubElement('div');
+let composeHeight = "500px";
+let main = body.appendNewElement('div');
 main.setHeight(composeHeight)
-	.setWidth("66%")
-	.setStyle('display', 'inline-block')
-	.setBorder()
-	.createSubElement('p')
-	.append('Main menu');
+    .setWidth("612px")
+    .setStyle('display', 'inline-block')
+    .setStyle('color', 'green')
+    .setBorder()
+    .appendElemSize()
+    .prependNewElement('p')
+    .append('Main menu');
 
-let subMain = body.createSubElement('div');
+let subMain = body.appendNewElement('div');
 subMain.setHeight(composeHeight)
-	.setStyle('display', 'inline-block')
-	.setBorder()
-	.createSubElement('p')
-	.setStyle("transform", "rotate(90deg) translateX(40px)")
-	.setStyle("overflow-wrap", "unset")
-	.append('Sub main')
+    .setWidth("30px")
+    .setStyle('display', 'inline-block')
+    .addClass("rotate")
+    .setEmpty();
 
-
-let side = body.createSubElement('div');
+let side = body.appendNewElement('div');
 side.setHeight(composeHeight)
-	.setWidth("20%")
-	.setStyle('display', 'inline-block')
-	.setBorder()
-	.createSubElement('p')
-	.append('Side bar');
+    .setWidth("266px")
+    .setStyle('display', 'inline-block')
+    .setStyle('color', 'blue')
+    .setBorder()
+    .appendElemSize()
+    .prependNewElement('p')
+    .append('Side bar');
 
-let subCompose = body.createSubElement('div');
-subCompose.setHeight("5%")
-	.setWidth("90%")
-	.createSubElement('p')
-	.append('Sub compose');
+let subCompose = body.appendNewElement('div');
+subCompose.setHeight("30px")
+    .setWidth("900px")
+    .setEmpty();
 
-let footer = body.createSubElement('div');
-footer.setHeight("10%")
-	.setWidth("90%")
-	.setBorder()
-	.createSubElement("p")
-	.append("Footer");
+let footer = body.appendNewElement('div');
+footer.setHeight("190px")
+    .setBorder()
+    .setWidth("900px")
+    .setStyle("color", "red")
+    .appendElemSize()
+    .prependNewElement('p')
+    .append("Footer");
